03a63b934dc8fc3fc714e97b572b16c3d58d6d0c
yomaswn <yaumashf@gmail.com>
2017-01-30 14:45:41 +0700

    beres seed data

---
+++ models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
